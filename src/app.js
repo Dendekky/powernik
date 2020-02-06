0ac3c70c3483bc80ac2bbae9d814d697e12e9e74
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2020-02-06 10:31:35 +0100

    chore: set up CRA for the powernik app

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
